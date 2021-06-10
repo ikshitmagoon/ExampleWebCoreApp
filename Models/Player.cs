413eafa580c896a27d4d2b0e7154ce71f7f333ee
Ikshit <ikshitmagoon99@gmail.com>
2021-06-10 15:13:49 +0530

    second commit

---
+++ Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppcoreGit1.Models
+{[Table("Palyer")]
+    public class Player
+    {
+        [Key]
+        public int PId { get; set; }
+        public string Pname { get; set; }
+        public string PTeam { get; set; }
+    }
+}
